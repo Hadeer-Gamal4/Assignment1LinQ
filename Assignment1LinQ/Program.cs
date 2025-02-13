aa8d9b676927797e8cd93416261901d75f818bdf
Hadeer Gamal Fayad Ahmed Ali <42110427.Hadeer@acu.edu.eg>
2025-02-13 20:00:49 +0200

    Add project files.

---
+++ Assignment1LinQ/Program.cs
@@ -0,0 +1,14 @@
+﻿using LinqDemo;
+using static LinqDemo.ListGenerator;
+namespace Assignment1LinQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = ProductList.Where(Product => Product.UnitsInStock == 0);
+            foreach (var item in result)
+                Console.WriteLine(result);
+        }
+    }
+}
